944c812a880eb0e32bf39e2cea746dd473cec349
So Jeong Park <psj6414@naver.com>
2021-02-03 06:19:56 +0900

    [그리디]큰 수의 법칙

---
+++ 큰수의법칙.py
@@ -0,0 +1,28 @@
+# 큰 수의 법칙
+# 주어진 배열의 주어진 숫자를 M번 더해 가장 큰 수 만드는 법칙, 단 동일인덱스에 해당하는 수가 연속 K번 초과해 더해질 수 없다.
+# 6 + 6+ 6 + 5+ 6 + 6 + 6 + 5
+# 배열의 크기 N, 숫자가 더해지는 횟수 M, 그리고 K가 주어질때 큰 수의 법칙에 따라 가장 큰 수를 출력하시오
+# 입력예시
+# 5 8 3
+# 2 4 5 4 6
+
+n, m, k = map(int, input().split())
+
+arr = list(map(int, input().split()))
+arr.sort()
+firstNum = arr[n-1] # 가장 큰 수
+secondNum = arr[n-2] # 그다음으로 큰 수
+
+sum = 0
+while True:
+    for i in range(k):
+        if m == 0:
+            break
+        sum += firstNum
+        m -= 1
+    if m == 0:
+        break
+    sum += secondNum
+    m -= 1
+
+print(sum)
