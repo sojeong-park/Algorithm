00f3fe956f9059b8402dc4dbfa426d723e668f35
So Jeong Park <psj6414@naver.com>
2021-02-10 21:34:11 +0900

    [DP] 동전2

---
+++ DP/B2294.py
@@ -0,0 +1,20 @@
+# 동전2
+# https://www.acmicpc.net/problem/2294
+
+n, k = map(int, input().split())
+
+arr = []
+for i in range(n):
+    arr.append(int(input()))
+
+d = [10001] * (k+1)
+d[0] = 0
+
+for i in range(n):
+    for j in range(arr[i], k+1):
+        d[j] = min(d[j], d[j - arr[i]]+1)
+
+if d[k] == 10001:
+    print(-1)
+else:
+    print(d[k])
