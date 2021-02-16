d280245dcb69a647bfe779f6ee624650ae5309c5
So Jeong Park <psj6414@naver.com>
2021-02-16 18:06:38 +0900

    [그래프] 기본적인 서로소 집합 알고리즘

---
+++ Graph/disjointSets.py
@@ -0,0 +1,37 @@
+# 기본적인 서로소 집합 알고리즘
+
+def findParent(parent, x):
+    if parent[x] != x:
+        return findParent(parent, parent[x])
+    return x
+
+def unionParent(parent, a, b):
+    a = findParent(parent, a)
+    b = findParent(parent, b)
+    if a < b:
+        parent[b] = a
+    else:
+        parent[a] = b
+
+# 노드의 개수와 간선의 개수 입력받기
+v, e = map(int, input().split())
+parent = [0] * (v+1)
+
+for i in range(1, v + 1):
+    parent[i] = i
+
+# union 연산 각각 수행
+for i in range(e):
+    a, b = map(int, input().split())
+    unionParent(parent, a, b)
+
+#각 원소가 속한 집합 출력
+print('각 원소가 속한 집합:', end='')
+for i in range(1, v+1):
+    print(findParent(parent, i), end = ' ')
+print()
+
+# 부모 테이블 내용 출력
+print('부모 테이블:', end='')
+for i in range(1, v+1):
+    print(parent[i], end=' ')
