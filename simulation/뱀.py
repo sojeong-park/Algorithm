6276b1305a539339b627464bfe29b0d027fae160
So Jeong Park <psj6414@naver.com>
2021-03-03 18:10:55 +0900

    [simulation] 뱀 - 본인 몸통과 만날 경우 종료 코드 추가

--- simulation/뱀.py
+++ simulation/뱀.py
@@ -3,58 +3,56 @@
 
 n = int(input())
 m = int(input())
-arr = [[0]*n for _ in range(n)]
-visits = [[0]*n for _ in range(n)]
+arr = [[0]*(n+1) for _ in range(n+1)] # 맵 정보
+
 for i in range(m):
     x, y = map(int, input().split())
     arr[x][y] = 1
 
 k = int(input())
-directions = [] # z 초일때 방향 전환한다.
+directions = [] # z 초일때 방향 전환한다. 방향 정보
 
-for i in range(k):
+for _ in range(k):
     z, direction = input().split()
     directions.append((int(z),direction))
 
-# 시작은 0,0에서 오른쪽 방향으로 이동.
-# 이동하다 directions 배열에 저장된 값과 만나면 방향 전환하여 그 방향으로 이동진행
-# 사과를 만나면 꼬리를 만들고
-# 사과를 못만나면 꼬리 길이 1로 만들기.
-# 범위에서 벗어나거나 자기 꼬리 만나면 끝!
-# 한칸 이동하고 끝나는 조건인지 체크.
-# 이동할때마다 count 세기
-# visits 배열로 방문 가능한지 아닌지 체크. 꼬리를 만났다면 끝내기 용도
+#d = [(-1,0), (0,1),(1,0),(0,-1)]
+dx = [0,1,0,-1]
+dy = [1,0,-1,0]
 
-d = [(-1,0), (0,1),(1,0),(0,-1)]
-def directionChange(count, head):
-    for time, direction in directions:
-        if count == time:
-            if direction == 'L':
-                head -= 1
-                if head < 0: head = 3
-                return head
-            else:
-                head += 1
-                if head > 3: head = 0
-                return head
-    return head
+def directionChange(direction, c):
+    if c == 'L':
+        direction = (direction-1) % 4
+    else:
+        direction = (direction+1) % 4
+    return direction
 
 def solution():
-    count = 0
-    x = 0
-    y = 0
-    head = 1 # 오른쪽 방향
+    count, x, y = 0, 1,1
+    arr[x][y] = 2 # 뱀이 존재하는 위치는 2로 표시
+    direction = 0 # 초기 오른쪽 방향
+    index = 0 # 다음에 회전할 정보
+    q = [(x, y)] # 뱀이 차지하고 있는 위치 정보
     while True:
-        if x >= 0 and x < n and y >= 0 and y < n: # x,y가 전부다 범위안에 존재
-            x += d[head][0]
-            y += d[head][1]
-            count += 1
-            # 벽에 부딪혔거나 자기 꼬리와 마주쳤거나 하면 break;
-            if x+d[head][0] < 0 and x+d[head][0] >= n and y+d[head][1] < 0 and y+d[head][1] >= n:
-                break;
-            head = directionChange(count, head)
+        nx = x + dx[direction]
+        ny = y + dy[direction]
+        if nx >= 1 and nx <= n and ny >= 1 and ny <= n and arr[nx][ny] != 2: # 맵의 범위안에 존재하고 뱀의 몸통이 없는 위치인경우
+            if arr[nx][ny] == 0: #사과가 없을 경우 꼬리 제거
+                arr[nx][ny] = 2 # 뱀의 몸통위치
+                q.append([nx, ny])
+                px, py = q.pop(0)
+                arr[px][py] = 0
+            if arr[nx][ny] == 1:
+                arr[nx][ny] = 2
+                q.append([nx,ny])
         else:
+            count += 1
             break
+        x, y = nx, ny
+        count += 1
+        if index < 1 and count == directions[index][0]:
+            direction = directionChange(direction, directions[index][1])
+            index += 1
     return count
 
 print(solution())
