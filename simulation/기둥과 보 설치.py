9f20f2c43075b37478db0ef2b2ebf12f57eb75ca
So Jeong Park <psj6414@naver.com>
2021-03-05 13:34:18 +0900

    [simulation] 기둥과 보 설치 - 정답코드 추가(solutions2())

--- simulation/기둥과 보 설치.py
+++ simulation/기둥과 보 설치.py
@@ -3,7 +3,8 @@
 n = 5
 build_frame = [[1,0,0,1],[1,1,1,1],[2,1,0,1],[2,2,1,1],[5,0,0,1],[5,1,0,1],[4,2,1,1],[3,2,1,1]]
 build_frame2 = [[0,0,0,1],[2,0,0,1],[4,0,0,1],[0,1,1,1],[1,1,1,1],[2,1,1,1],[3,1,1,1],[2,0,0,0],[1,1,1,0],[2,2,0,1]]
-def solution(n, build_frame):
+
+def solution(n, build_frame): # 오답!
     arr = [[2]*(n+1) for _ in range(n+1)]
     for x, y, a, b in build_frame:
         if a == 0: # 기둥
@@ -32,3 +33,29 @@ def solution(n, build_frame):
     return answer
 
 print(solution(n, build_frame))
+
+def checkPosiible(answer):
+    for x, y, stuff in answer:
+        if stuff == 0: # 설치를 기둥에 한 경우
+            if y==0 or [x-1, y, 1] in answer or [x,y,1] in answer or [x,y,-1,0]:
+                continue
+            return False
+        elif stuff == 1: # 설치된것이 보인 경우
+            if [x,y-1, 0] in answer or [x+1, y-1, 0] in answer or [x-1,y,1] in answer or [x+1, y, 1] in answer:
+                continue
+            return False
+        return True
+
+def solution2(n, build_frame): # 정답!
+    answer=[]
+    for frame in build_frame:
+        x, y, stuff, operate = frame
+        if operate ==0 : # 삭제
+            answer.remove([x,y,stuff]) #삭제하고
+            if not checkPosiible(answer): # 구조물이 정상이 아니라면
+                answer.append([x,y,stuff]) # 복구
+        if operate == 1: # 설치
+            answer.append([x,y,stuff])
+            if not checkPosiible(answer):
+                answer.remove([x,y,stuff])
+    return sorted(answer)
