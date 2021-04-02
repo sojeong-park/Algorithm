bd6a158ce61ca177d939eb6be6d6f2e0e7732506
So Jeong Park <psj6414@naver.com>
2021-04-02 23:26:14 +0900

    [BFS] 백준 인구이동

--- DFS,BFS/B16234.py
+++ DFS,BFS/B16234.py
@@ -1,22 +1,44 @@
 # 인구이동 https://www.acmicpc.net/problem/16234
 from _collections import deque
 
-def bfs(gragh, start, visited):
-    que = deque([start])
-    visited[start] = True
-    # que가 null일때까지 반복
-    while que:
-        v = que.popleft()
-        print(v, end=' ')
-        for i in gragh[v]:
-            if not visited[i]:
-                que.append(i)
-                visited[i] = True
-
 n, L, m = map(int, input().split())
 
-a = []
-visit = [[0]*n for _ in range(n)]
+graph = []
 
 for i in range(n):
-    a.append(list(map(int, input().split())))
+    graph.append(list(map(int, input().split())))
+
+dx = [-1, 0, 1, 0]
+dy = [0, -1, 0, 1]
+
+result = 0
+
+def process(x, y, index):
+    united = []
+    united.append((x, y))
+
+    q = deque()
+    q.append((x, y))
+
+    united[x][y] = index
+    summary = graph[x][y]
+    count = 1
+
+    while q:
+        x, y = q.popleft()
+        for i in range(4):
+            nx = x + dx[i]
+            ny = y + dy[i]
+            if 0 <= nx < n and 0 <=ny < n and united[nx][ny] == -1:
+                if l <= abs(graph[nx][ny] - graph[x][y]) <= r:
+                    q.append((nx, ny))
+                    united[nx][ny] = index
+                    summary += graph[nx][ny]
+                    count += 1
+                    united.append((nx, ny))
+    for i, j in united:
+        graph[i][j] = summary
+    return count
+
+total_count = 0
+
